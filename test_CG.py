9ebd4b7740fa45ed4885efc032a6c0aebd783428
aitzkora <marc.fuentes@inria.fr>
2012-09-13 14:05:43 +0200

    modify CG to depends only on trilinos

--- test_CG.py
+++ test_CG.py
@@ -1,32 +1,17 @@
-from PyTrilinos import Epetra, EpetraExt
+from PyTrilinos import Epetra, EpetraExt, TriUtils
 from EpetraCG import cg
-from scipy2Trilinos import scipy_csr_matrix2CrsMatrix
 import numpy as np
-from scipy.sparse import csr_matrix, spdiags, rand
 
-np.random.seed(12)
 N=100
-#data=np.array([[-1],[2],[-1]])*np.ones((1,N))
-#diags = np.array([-1,0,1])
-Z = rand(N,N, 0.4)
-Z = Z + Z.T 
-#A_csr=spdiags(data, diags, 4, 4).tocsr()
-A_csr = Z.tocsr() 
 mycomm = Epetra.PyComm()
-A = scipy_csr_matrix2CrsMatrix(A_csr, mycomm)
+gal = TriUtils.CrsMatrixGallery('laplace_3d', mycomm)
+gal.Set('nx', N)
+gal.Set('ny', N)
+gal.Set('nz', N)
 
-#initial guess
-x0=np.ones((N),dtype='float')
-X = Epetra.Vector(A.DomainMap())
-for ii in range(X.MyLength()):
-    i=  X.Map().GID(ii)
-    X[ii] = x0[i] 
-# rhs
-F=np.ones((N),dtype='float')
-Y = Epetra.Vector(A.RangeMap())
-for ii in range(Y.MyLength()):
-    i=  Y.Map().GID(ii)
-    Y[ii] = F[i]
+A = gal.GetMatrix()
+Y = gal.GetStartingSolution()
+X = gal.GetRHS()
+
 
 sol = cg(A, X, Y, 1e-6, 2000)
-
